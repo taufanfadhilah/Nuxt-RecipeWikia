1342dd45065bb6e663921a57ed40676cbf881a5d
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2020-01-05 07:18:17 +0700

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <div class="card">
+          <div class="card-body">
+            <h4>Welcome to Recipe Wikia</h4>
+            <hr>
+            <p class="subtitle">Please Login to Continue</p>
+            <form action="">
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control">
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control">
+              </div>
+              <div class="form-group">
+                <button type="button" class="btn btn-primary btn-sm">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+body {
+  background: #e65100;
+}
+.card {
+  margin-top: 50px;
+}
+.subtitle {
+  font-size: 12px;
+  margin: 0px;
+  font-style: italic;
+  color: #ff9100
+}
+.btn {
+  width: 100px;
+}
+</style>
